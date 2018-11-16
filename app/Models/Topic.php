3abd69dc7ba2ec12bcbf45f723243374b47c02d4
zgf <277845885@qq.com>
2018-11-16 10:43:25 +0800

    1

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -6,6 +6,11 @@ class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'category_id', 'excerpt', 'slug'];
 
+    public function replies()
+    {
+        return $this->hasMany(Reply::class, 'topic_id', 'id');
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class, 'category_id', 'id');
