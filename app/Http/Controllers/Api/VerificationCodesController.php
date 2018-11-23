67d99cb5b60a4a8e5f72d3fb46bdb91f5c37a61b
zgf123 <277845885@qq.com>
2018-11-23 04:58:54 +0000

    1

---
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Overtrue\EasySms\EasySms;
+use App\Http\Requests\Api\VerificationCodeRequest;
+
+class VerificationCodesController extends Controller
+{
+    public function store(verificationCodeRequest $request, EasySms $easysms)
+    {
+        $phone = $request->phone;
+        if(config('app.env') == 'local'){
+            $code = '1231';
+        }else{
+            $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+
+            try {
+                $result = $easysms->send($phone, [
+                    'content' => "【Lbbs社区】您的验证码是{$code}。如非本人操作，请忽略本短信"
+                ]);
+            } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception){
+                dd('发送失败');
+            }
+        }
+
+        $key = 'verificationCode_'.str_random(15);
+        $expired = now()->addMinutes(10);
+        
+        \Cache::put($key, ['phone'=>$phone, 'code'=>$code], $expired);
+        
+        return $this->response->array([
+            'key' => $key,
+            'expired_at' => $expired->toDateTimeString()
+        ])->setStatusCode(201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,18 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+*/
+
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', [
+    'namespace' => 'App\Http\Controllers\Api'
+], function($api) {
+    // 短信验证码
+    $api->post('verificationCodes', 'VerificationCodesController@store')
+        ->name('api.verificationCodes.store');
 });
